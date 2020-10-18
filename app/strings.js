eb5f389d063f2effb04910ea9249fd3e6bbe6672
Hannah Lima <nanaa_lima@hotmail.com>
2020-10-18 14:29:27 -0500

    string manipulation

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,58 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
+  reduceString: function (str, amount) {
+    let result = str[0];
+    let dictionary = {};
+    dictionary[result] = 1;
 
+    for (let index = 1; index < str.length; index++) {
+      const previousLetter = str[index - 1];
+      const currentLetter = str[index];
+
+      if (previousLetter !== currentLetter) {
+        dictionary[previousLetter] = 0;
+      }
+      dictionary[currentLetter] = dictionary[currentLetter]
+        ? dictionary[currentLetter] + 1
+        : 1;
+      result += dictionary[currentLetter] <= amount ? currentLetter : "";
+    }
+
+    return result;
   },
 
-  wordWrap: function(str, cols) {
+  wordWrap: function (str, cols) {
+    let result = "";
 
+    let wordArray = str.split(" ");
+
+    result = wordArray[0];
+
+    for (let index = 1; index < wordArray.length; index++) {
+      const currentWord = wordArray[index];
+
+      if (currentWord.length > cols) {
+        result += "\n" + currentWord;
+      } else {
+        if (currentWord.length + result.length > cols) {
+          result += "\n" + currentWord;
+        } else {
+          result += " " + currentWord;
+        }
+      }
+    }
+
+    return result;
   },
 
-  reverseString: function(str) {
+  reverseString: function (str) {
+    let result = "";
 
-  }
+    for (let index = str.length - 1; index >= 0; index--) {
+      const currentLetter = str[index];
+      result = result + currentLetter;
+    }
+    return result;
+  },
 };
